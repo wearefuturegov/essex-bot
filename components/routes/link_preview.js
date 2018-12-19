97813d4450e11dc0197ffee744ee910e0414fe63
Glitch (botkit-web) <none>
2018-12-19 10:26:33 +0000

    Add baseURL to link preview

--- components/routes/link_preview.js
+++ components/routes/link_preview.js
@@ -1,10 +1,16 @@
 var og = require('open-graph');
+var URL = require('url').URL;
 
 module.exports = function(webserver, controller) {
   
   webserver.post('/link_preview', function(req, res) {
-    og(req.body.url, function(err, meta){
+    var url = req.body.url;
+    og(url, function(err, meta){
       res.setHeader('Content-Type', 'application/json');
+      var baseURL = new URL(url).origin;
+      if (meta.image !== undefined && !meta.image.url.startsWith(baseURL)) {
+        meta.image.url = baseURL + meta.image.url;
+      }
       res.send(JSON.stringify(meta));
     })
   });
