855f8c8bac907527f4980c584bb4bf053d9cc6e3
Glitch (botkit-web) <none>
2018-12-20 10:29:18 +0000

    Clean up link preview

--- components/routes/link_preview.js
+++ components/routes/link_preview.js
@@ -1,16 +1,30 @@
-var og = require('open-graph');
+var og = require('open-graph-scraper');
 var URL = require('url').URL;
 
 module.exports = function(webserver, controller) {
   
   webserver.post('/link_preview', function(req, res) {
     var url = req.body.url;
-    og(url, function(err, meta){
-      res.setHeader('Content-Type', 'application/json');
-      var baseURL = new URL(url).origin;
-      if (meta.image !== undefined && !meta.image.url.startsWith('http')) {
-        meta.image.url = baseURL + meta.image.url;
+    var options = {
+      url: url,
+      headers: { 
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
       }
+    }
+    og(options, function(err, result){
+      if (!err) {
+        meta = result.data;
+        var baseURL = new URL(url).origin;
+        if (Array.isArray(meta.ogImage)) {
+          meta.ogImage = meta.ogImage[0]
+        }
+        if (meta.ogImage !== undefined && !meta.ogImage.url.startsWith('http')) {
+          meta.ogImage.url = baseURL + meta.ogImage.url;
+        }
+      } else {
+        meta = {}
+      }
+      res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify(meta));
     })
   });
